3a569cff8e0a973e6d6b0f75000024d7215e8fd2
Guilherme Pereira santana <guilhermesantana159357@hotmail.com>
2023-04-07 08:56:01 -0300

    Teste unitario
    Teste unitario

---
+++ ProjetoPadraoDotnetCore/Testes/UsuarioTeste/UsuarioFixture.cs
@@ -0,0 +1,14 @@
+﻿using Aplication.Utils.Objeto;
+
+namespace Tests.UsuarioTeste;
+
+public class UsuarioFixture
+{
+    public ValidationResult RetornarInvalidoResult()
+    {
+        var result = new ValidationResult();
+        result.LErrors.Add("Email já informado");
+        
+        return result;
+    }
+}
---
+++ ProjetoPadraoDotnetCore/Testes/UsuarioTeste/UsuarioTeste.cs
@@ -0,0 +1,62 @@
+﻿using Aplication.Controllers;
+using Aplication.Models.Request.Usuario;
+using Aplication.Validators.Usuario;
+using AutoMoq;
+using Bogus;
+using Domain.Interfaces;
+using Domain.Services;
+using Infraestrutura.Entity;
+using Moq;
+using Xunit;
+using ValidationResult = Aplication.Utils.Objeto.ValidationResult;
+
+namespace Tests.UsuarioTeste;
+
+public class UsuarioTeste
+{
+    private AutoMoqer _mock;
+    
+    public UsuarioTeste()
+    {
+        _mock = new AutoMoqer();
+    }
+
+    [Fact]
+    public void UsuarioValidoCadastro()
+    {
+        // Arrange
+        var user = new Faker<UsuarioRequest>()
+            .CustomInstantiator(f => new UsuarioRequest()
+            {
+                IdUsuario = null,
+                Nome = f.Name.FirstName(),
+                Bairro = f.Locale,
+                DataNascimento = f.Date.Recent(),
+                Cep = f.Locale,
+                Cidade = f.Locale,
+                Cpf = f.Name.FullName(),
+                Email = f.Locale,
+                Estado = f.Address.City(),
+                Foto = f.Phone.PhoneNumber(),
+                Rua = f.Locale
+            });
+            
+        _mock.Create<UsuarioApp>();
+        _mock.GetMock<IUsuarioService>()
+            .Setup(x => x.GetAllList())
+            .Returns(new List<Usuario>());
+        
+        _mock.GetMock<IUsuarioValidator>()
+            .Setup(x => x.ValidacaoCadastro(It.IsAny<UsuarioRequest>()))
+            .Returns(new ValidationResult());
+        
+        var customerService = _mock.Resolve<UsuarioApp>();
+
+        // Act
+        var result = customerService.Cadastrar(user);
+
+        //Assert
+        Assert.True(result.IsValid());
+
+    }
+}
